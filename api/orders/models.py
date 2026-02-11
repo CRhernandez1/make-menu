b2844686dd027ecf69ca127ae0797d9c88048077
Cristian Reyes Hernández <cristianreyesh01@gmail.com>
2026-02-11 11:37:15 +0000

    Refactor models: added Category and Allergens entities
    Co-authored-by: samuelcrz30 <samuelcruz9p@gmail.com>
    Co-authored-by: dettriva <pe581490@gmail.com>

--- api/orders/models.py
+++ api/orders/models.py
@@ -17,7 +17,8 @@ class Order(models.Model):
     )
     placed_at = models.DateTimeField(auto_now_add=True)
     paid = models.BooleanField(default=False)
-    closed_at = models.DateTimeField()
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    closed_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
         return f'{self.pk}: {self.get_status_display()}'
@@ -29,12 +30,12 @@ class OrderDetail(models.Model):
     )
 
     product = models.ForeignKey(
-        'products.product', related_name='orders_details', on_delete=models.PROTECT
+        'products.Product', related_name='orders_details', on_delete=models.PROTECT
     )
 
     price = models.DecimalField(max_digits=6, decimal_places=2)
     notes = models.TextField(blank=True)
-    quantity = models.PositiveSmallIntegerField()
+    quantity = models.PositiveSmallIntegerField(default=1)
 
     def __str__(self):
         return f'{self.pk}: {self.price}'
--- api/products/models.py
+++ api/products/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
 
-class Product(models.Model):
-    class Category(models.TextChoices):
-        DRINKS = 'drinks'
-        DESSERTS = 'desserts'
-        FOOD = 'food'
+class Category(models.Model):
+    establishment = models.ForeignKey(
+        'establishments.Establishment', related_name='categories', on_delete=models.CASCADE
+    )
+    name = models.CharField()
 
+    def __str__(self):
+        return f'{self.name} ({self.establishment.name})'
+
+
+class Product(models.Model):
     establishment = models.ForeignKey(
         'establishments.Establishment', related_name='products', on_delete=models.CASCADE
     )
-    category = models.CharField(choices=Category)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.PROTECT)
     name = models.CharField()
     description = models.TextField(blank=True)
     product_image = models.ImageField(upload_to='products', default='products/noproduct.png')
@@ -22,12 +27,27 @@ class Product(models.Model):
 
 
 class Ingredient(models.Model):
+    class Type(models.TextChoices):
+        MEAT = 'meat'
+        FISH = 'fish'
+        VEGETABLES = 'vegetables'
+        DAIRY = 'dairy'
+        BAKERY = 'bakery'
+        SAUCES = 'sauces'
+        DRINKS = 'drinks'
+        OTHER = 'other'
+
     establishment = models.ForeignKey(
         'establishments.Establishment', related_name='ingredients', on_delete=models.CASCADE
     )
+    allergens = models.ManyToManyField(
+        'products.Allergen',
+        blank=True,
+        related_name='ingredients',
+    )
+    ingredient_type = models.CharField(choices=Type.choices, default=Type.OTHER)
     name = models.CharField()
     description = models.TextField(blank=True)
-    # en un futuro: allergens = models.CharField(blank=True)
     available = models.BooleanField(default=True)
 
     def __str__(self):
@@ -36,12 +56,11 @@ class Ingredient(models.Model):
 
 class Component(models.Model):
     class Unity(models.TextChoices):
-        UNITY = 'ud'
-        GRAMS = 'gr'
-        KILOGRAMS = 'kg'
-        LITERS = 'l'
-        MILILITERS = 'ml'
-        CENTILITERS = 'cl'
+        UNITY = 'ud', 'Unidades'
+        GRAMS = 'gr', 'Gramos'
+        KILOGRAMS = 'kg', 'Kilos'
+        LITERS = 'l', 'Litros'
+        MILILITERS = 'ml', 'Mililitros'
 
     product = models.ForeignKey(
         'products.product', related_name='components', on_delete=models.PROTECT
@@ -49,5 +68,16 @@ class Component(models.Model):
     ingredient = models.ForeignKey(
         'products.ingredient', related_name='components', on_delete=models.PROTECT
     )
-    quantity = models.PositiveSmallIntegerField()
-    unity = models.CharField(choices=Unity, max_length=2)
+    quantity = models.DecimalField(max_digits=6, decimal_places=2)
+    unity = models.CharField(choices=Unity.choices, max_length=2)
+    removable = models.BooleanField(default=True)
+
+
+class Allergen(models.Model):
+    name = models.CharField(unique=True)
+    icon = models.ImageField(
+        upload_to='allergens/', default='allergens/noallergen.png', blank=True, null=True
+    )
+
+    def __str__(self):
+        return self.name
