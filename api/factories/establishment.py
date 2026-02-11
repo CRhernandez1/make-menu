2f8963d4cbd7fe2f5d8ca35fd584d44e6d945c35
Cristian Reyes Hernández <cristianreyesh01@gmail.com>
2026-02-11 18:34:47 +0000

    tailwind added
    Co-authored-by: samuelcrz30 <samuelcruz9p@gmail.com>
    Co-authored-by: dettriva <pe581490@gmail.com>

--- api/factories/establishment.py
+++ api/factories/establishment.py
@@ -1,12 +1,11 @@
 import factory
 from django.utils import timezone
-from faker import Faker
 
 from .data import LOCAL_NAMES
 from .extras import UniqueFaker
 
+factory.Faker._DEFAULT_LOCALE = 'es_ES'
 TZ = timezone.get_current_timezone()
-fake = Faker('es_ES')
 
 
 class EstablishmentFactory(factory.django.DjangoModelFactory):
@@ -16,13 +15,11 @@ class EstablishmentFactory(factory.django.DjangoModelFactory):
 
     name = factory.Faker('random_element', elements=LOCAL_NAMES)
     legal_name = factory.LazyAttribute(lambda o: f'{o.name} S.L.')
-    # Usamos UniqueFaker porque el modelo exige unique=True
     # 'bothify' crea una letra y 8 números (formato común de CIF)
     cif = UniqueFaker('bothify', text='?########')
     description = factory.Faker('paragraph', nb_sentences=3)
     zip_code = factory.Faker('postcode')
     city = factory.Faker('city')
     address = factory.Faker('address')
-    # Limitamos a 16 caracteres como indica tu modelo
     phone = factory.Faker('numerify', text='###########')
     opened = factory.Faker('boolean', chance_of_getting_true=50)
